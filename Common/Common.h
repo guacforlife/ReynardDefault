0d8242c72c111f50e0409ad9a4ce70ba84070bb5
guacforlife <264412991+guacforlife@users.noreply.github.com>
2026-04-14 13:47:50 +0100

    Add rootful build variant; support unc0ver and palera1n rootful
    - Makefile: add rootless/rootful/both targets; build both variants
      from the same source via env -i + FINALPACKAGE=1
    - Common.h: conditionally import rootless.h via __has_include
    - control: default Architecture to iphoneos-arm; rootless builds
      override to iphoneos-arm64 via THEOS_PACKAGE_SCHEME
    - README: document dual-scheme build and tested environment
    
    Closes #1

--- Common/Common.h
+++ Common/Common.h
@@ -1,6 +1,9 @@
+#if __has_include(<rootless.h>)
 #import <rootless.h>
-
 #define jbRootPath(path) ROOT_PATH_NS(path)
+#else
+#define jbRootPath(path) (path)
+#endif
 
 static NSString *const kSuiteName = @"com.guacforlife.reynarddefaultprefs";
 static NSString *const kEnabledKey = @"enabled";
